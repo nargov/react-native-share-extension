421b9a6c6ce350523240704f089a6dd38c118e0a
Nimrod Argov <nimrod@wishkeeper.co>
2019-10-15 11:45:07 +0300

    update to android x and react-native >0.60

---
+++ android/build/generated/source/buildConfig/debug/com/alinz/parkerdan/shareextension/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alinz.parkerdan.shareextension;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.alinz.parkerdan.shareextension";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.alinz.parkerdan.shareextension";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
